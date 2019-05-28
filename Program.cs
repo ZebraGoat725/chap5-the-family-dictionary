b8c0f86bc69f5951b06b7d02fe41e07434a67790
Alex Thacker <thacker.alex@yahoo.com>
2019-05-28 15:39:28 -0500

    done

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FamilyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, string>> myFamily = new Dictionary<string, Dictionary<string, string>>();
+
+            myFamily.Add("sister", new Dictionary<string, string>(){
+                {"name", "Krista"},
+                {"age", "42"}
+            });
+
+            myFamily.Add("brother", new Dictionary<string, string>(){
+                {"name", "bo"},
+                {"age", "33"}
+            });
+
+            myFamily.Add("pet", new Dictionary<string, string>(){
+                {"name", "dog"},
+                {"age", "1"}
+            });
+
+            foreach (KeyValuePair<string, Dictionary<string, string>> familyMember in myFamily)
+            {
+                Console.WriteLine($"{familyMember.Value["name"]} is my {familyMember.Key} and is {familyMember.Value["age"]} years old");
+                // Krista is my sister and is 42 years old
+                // foreach(KeyValuePair <string, string> member in familyMember.Value){
+                //     Console.WriteLine(member["name"]); 
+                // }
+            }
+        }
+    }
+}
